ac6bd29b4ee49cb73dda4a5142127f5d814928e5
myd510 <57065583+myd510@users.noreply.github.com>
2021-12-19 00:57:16 +0800

    Update VideoWallpaper.py

--- VideoWallpaper.py
+++ VideoWallpaper.py
@@ -1,34 +1,33 @@
-from win32gui import FindWindow, FindWindowEx, ShowWindow, SendMessageTimeout, SetParent, EnumWindows
+from win32gui import FindWindow, FindWindowEx, ShowWindow, SendMessageTimeout, SetParent, EnumWindows, GetWindowText
 from win32con import SW_HIDE, SMTO_ABORTIFHUNG
-from win32process import CreateProcess, STARTUPINFO
-from time import sleep
+
 
 def _MyCallback(hwnd, extra):#遍历窗口函数的回调函数（提前return退出遍历会报错）
-    #查找当前被遍历顶层窗口包含的桌面图标所在窗口
+    #当前窗口中查找图标窗口
     icon_window = FindWindowEx(hwnd, None, "SHELLDLL_DefView", None)
-    if(icon_window!=0):#当前被遍历顶层窗口包含桌面图标所在窗口
-        #查找下一个类名为WorkerW的顶层窗口（即静态壁纸所在窗口）
-        workerw = FindWindowEx(None, hwnd, "WorkerW", None)
-        #隐藏静态壁纸所在窗口
-        ShowWindow(workerw, SW_HIDE)
+    if(icon_window!=0):#当前窗口包含图标窗口
+        #查找静态壁纸窗口并保存
+        extra[0] = FindWindowEx(None, hwnd, "WorkerW", None)
 
-def RunVideoWallpaper(video_path, custom_settings='', ffplay_path=r".\ffmpeg-win64\ffplay.exe"):#设置视频壁纸
-    #查找桌面窗口
-    desktop_window = FindWindow("Progman", "Program Manager")
-    #默认播放设置：全屏，无限循环，无输出
-    cmdline = "-fs -loop 0 {} \"{}\" -loglevel quiet".format(custom_settings, video_path)
-    #创建播放器进程
-    CreateProcess(ffplay_path, cmdline, None, None, 0, 0, None, None, STARTUPINFO())
-    while(True):#等待播放器窗口创建完毕
-        #查找播放器窗口
-        player_window = FindWindow("SDL_app", video_path)
-        if(player_window!=0):#找到播放器窗口
-            #视频窗口窗口原点会不在00，sleep一个极短的时间可以解决
-            sleep(0.01)
-            break
-    #核心语句，向desktop_window发送0x52C启用Active Desktop
-    SendMessageTimeout(desktop_window, 0x52C, 0, 0, SMTO_ABORTIFHUNG, 100)
-    #将player_window设置为desktop_window的子窗口
-    SetParent(player_window, desktop_window)
-    #因为有两个同类同名的WorkerW窗口，所以遍历所以顶层窗口
-    EnumWindows(_MyCallback, None)
+def RunVideoWallpaper(player_window_handel):#设置视频壁纸
+    if(player_window_handel!=0):
+        #查找桌面窗口
+        desktop_window_handel = FindWindow("Progman", "Program Manager")
+        #设置player_window为desktop_window的子窗口
+        SetParent(player_window_handel, desktop_window_handel)
+        #核心语句，向desktop_window发送0x52C启用Active Desktop
+        SendMessageTimeout(desktop_window_handel, 0x52C, 0, 0, SMTO_ABORTIFHUNG, 100)
+        #因为有两个同类同名的WorkerW窗口，所以遍历所以顶层窗口
+        workerw=[0]
+        EnumWindows(_MyCallback, workerw)
+        #获取player_windows名称
+        player_windows_name = GetWindowText(player_window_handel)
+        while(True):#防止win+tab将player_window发送到图标窗口的父窗口(原因不明)及SetParent导致静态壁纸窗口重新出现
+            #隐藏静态壁纸窗口
+            ShowWindow(workerw[0], SW_HIDE)
+            #判断player_window是否在desktop_window下
+            player_under_desktop = FindWindowEx(desktop_window_handel, None, "SDL_app", player_windows_name)
+            if(player_under_desktop==0):#如果player_window位置不正确
+                #将player_window设置为desktop_window的子窗口
+                SetParent(player_window_handel, desktop_window_handel)
+
