a57404f1d06bcea479a98b49c4a99fa3f10575a0
myd510 <57065583+myd510@users.noreply.github.com>
2021-12-18 13:07:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     #自定义播放设置：静音
     custom_settings = '-an'
     #开始视频壁纸
-    RunVideoWallpaper(video_path, custom_settings)
+    VideoWallpaper.RunVideoWallpaper(video_path, custom_settings)
     #暂停程序
     input()
