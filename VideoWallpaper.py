ef0985c24c1afc101f532ba4319c083059cb8eeb
myd510 <57065583+myd510@users.noreply.github.com>
2022-08-02 17:30:54 +0800

    Update VideoWallpaper.py

--- VideoWallpaper.py
+++ VideoWallpaper.py
@@ -1,23 +1,23 @@
 from win32gui import FindWindow, FindWindowEx, ShowWindow, SendMessageTimeout, SetParent, EnumWindows, GetWindowText
-from win32con import SW_HIDE, SMTO_ABORTIFHUNG
+from win32con import SW_HIDE, SMTO_ABORTIFHUNG,SW_SHOW
 
 
 def _MyCallback(hwnd, extra):#遍历窗口函数的回调函数（提前return退出遍历会报错）
     #当前窗口中查找图标窗口
     icon_window = FindWindowEx(hwnd, None, "SHELLDLL_DefView", None)
-    if(icon_window!=0):#当前窗口包含图标窗口
+    if(icon_window):#当前窗口包含图标窗口
         #查找静态壁纸窗口并保存
         extra[0] = FindWindowEx(None, hwnd, "WorkerW", None)
 
 def RunVideoWallpaper(player_window_handel):#设置视频壁纸
-    if(player_window_handel!=0):
+    if(player_window_handel):
         #查找桌面窗口
         desktop_window_handel = FindWindow("Progman", "Program Manager")
         #设置player_window为desktop_window的子窗口
         SetParent(player_window_handel, desktop_window_handel)
         #核心语句，向desktop_window发送0x52C启用Active Desktop
-        SendMessageTimeout(desktop_window_handel, 0x52C, 0, 0, SMTO_ABORTIFHUNG, 500)#如果报TimeOut,增加延时
-        #因为有两个同类同名的WorkerW窗口，所以遍历所以顶层窗口
+        SendMessageTimeout(desktop_window_handel, 0x52C, 0, 0, SMTO_ABORTIFHUNG, 500)
+        #因为有两个同类同名的WorkerW窗口，所以遍历所有顶层窗口
         workerw=[0]
         EnumWindows(_MyCallback, workerw)
         #获取player_windows名称
